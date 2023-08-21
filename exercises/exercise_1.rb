3bb003b0ef7688d14f5887750cc3bf293491528d
Natasha Chiu <chiuchiu07@gmail.com>
2023-08-22 02:54:15 +0800

    ex1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,12 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+burnaby = Store.create(name: 'Burnaby', annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+
+richmond = Store.create(name: 'Richmond', annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+
+gastown = Store.create(name: 'Gastown', annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+puts Store.count
+
+# CREATE TABLE "stores" ("id" bigserial primary key, "name" character varying, "annual_revenue" integer, "mens_apparel" boolean, "womens_apparel" boolean, "created_at" timestamp(6) NOT NULL, "updated_at" timestamp(6) NOT NULL)
